26971bdcca51ac33a6bd77140305dd86d56a8bc2
Victory Ajigbotosho <ajigbotoshovictory@gmail.com>
2024-03-20 21:54:00 +0100

    mime

--- lasustechrepo/lasustechrepo/settings.py
+++ lasustechrepo/lasustechrepo/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import mimetypes 
+  
+mimetypes.add_type("text/css", ".css", True)
+
 load_dotenv()
 
 
