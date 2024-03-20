f3c6016b538350d115e7c1d203f2754718c19854
Victory Ajigbotosho <ajigbotoshovictory@gmail.com>
2024-03-20 19:51:01 +0100

    mk

--- lasustechrepo/build_files.sh
+++ lasustechrepo/build_files.sh
@@ -3,5 +3,6 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 pip install django
 # make migrations
+python3.9 manage.py makemigrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
